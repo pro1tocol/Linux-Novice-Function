607bbe8b6661e6fda31fa6a7577cf7083fe9ae80
dot1q <82779435+pro1tocol@users.noreply.github.com>
2022-11-07 19:16:43 +0800

    Update Backlight.sh

--- Script/Backlight.sh
+++ Script/Backlight.sh
@@ -1,1 +1,6 @@
+echo "Current maximum brightness: "
+cat /sys/class/backlight/intel_backlight/max_brightness
+
+echo "Adjust the brightness to: "
 echo 1000 > /sys/class/backlight/intel_backlight/brightness
+cat /sys/class/backlight/intel_backlight/brightness
