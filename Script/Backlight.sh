15faaaf568fb7096c478e9d847605423277e972a
dot1q <82779435+pro1tocol@users.noreply.github.com>
2022-12-07 19:07:12 +0800

    Update Backlight.sh

--- Script/Backlight.sh
+++ Script/Backlight.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 #by C4ry_Anaymouns build
-echo " Instructions: sh Backlight.sh 1000 "
+echo ' Instructions: sh Backlight.sh 200 '
+echo " ____             _    _ _       _     _   "
+echo "| __ )  __ _  ___| | _| (_) __ _| |__ | |_ "
+echo "|  _ \ / _` |/ __| |/ / | |/ _` | '_ \| __|"
+echo "| |_) | (_| | (__|   <| | | (_| | | | | |_ "
+echo "|____/ \__,_|\___|_|\_\_|_|\__, |_| |_|\__|"
+echo "                           |___/           "
 
 
 echo " \n maximum-brightness: \c " && {cat /sys/class/backlight/intel_backlight/max_brightness}
 
-echo " \n Adjust the brightness to: \c" && echo $1 > /sys/class/backlight/intel_backlight/brightness && cat /sys/class/backlight/intel_backlight/brightness
+echo " \n Adjust the brightness to: \c"
+echo $1 > /sys/class/backlight/intel_backlight/brightness && cat /sys/class/backlight/intel_backlight/brightness
