e94898d3238d37726cf4b8335784ff72d4da6c31
dot1q <82779435+pro1tocol@users.noreply.github.com>
2022-12-07 19:14:15 +0800

    Update Backlight.sh

--- Script/Backlight.sh
+++ Script/Backlight.sh
@@ -3,7 +3,7 @@
 echo ' Instructions: sh Backlight.sh 200 '
 echo " ____             _    _ _       _     _   "
 echo "| __ )  __ _  ___| | _| (_) __ _| |__ | |_ "
-echo "|  _ \ / _` C 4 ry_Anaymouns _` | '_ \| __|"
+echo "|  _ \ / _  C 4 ry_Anaymouns _  | '_ \| __|"
 echo "| |_) | (_| | (__|   <| | | (_| | | | | |_ "
 echo "|____/ \__,_|\___|_|\_\_|_|\__, |_| |_|\__|"
 echo "                           |___/           "
